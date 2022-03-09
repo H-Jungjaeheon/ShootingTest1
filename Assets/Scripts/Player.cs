114b123833867c191946312943692eb987c4daae
H-Jungjaeheon <jjhking222@naver.com>
2022-03-09 22:50:20 +0900

    Map1
    Map1.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -23,11 +23,10 @@ public class Player : MonoBehaviour
     }
     void Move()
     {
-        float Horizontals = Input.GetAxis("Horizontal");
-        float Verticals = Input.GetAxis("Vertical");
-        Vector3 Position = transform.position;
-        Position.x += Speed * Horizontals * Time.deltaTime;
-        Position.y += Speed * Verticals * Time.deltaTime;
-        transform.position = Position;
+        float Horizontal = Speed * Time.deltaTime * Input.GetAxis("Horizontal");
+        float Vertical = Speed * Time.deltaTime * Input.GetAxis("Vertical");
+        transform.Translate(Horizontal, 0, Vertical);
+        rigid.velocity = Vector3.zero;
+        rigid.angularVelocity = Vector3.zero;
     }
 }
