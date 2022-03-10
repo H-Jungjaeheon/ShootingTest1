f619a0da27a1dd9e4671fb7f1d65bb44ec4a86e2
H-Jungjaeheon <jjhking222@naver.com>
2022-03-10 18:10:30 +0900

    BackGroundScroll
    BackGroundScroll1

--- Assets/Scripts/BackGroundScroll.cs
+++ Assets/Scripts/BackGroundScroll.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class BackGroundScroll : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float MoveSpeed;
+    [SerializeField] GameObject[] BackGround;
+    
     void Start()
     {
         
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        ScrollMove();
+    }
+    void ScrollMove()
+    {
+        float Z = MoveSpeed * Time.deltaTime;
+        BackGround[0].transform.Translate(0, 0, Z);
+        BackGround[1].transform.Translate(0, 0, Z);
+        if (BackGround[0].transform.position.z <= BackGround[2].transform.position.z)
+        {
+            BackGround[0].transform.position = BackGround[3].transform.position;
+        }
+        else if (BackGround[1].transform.position.z <= BackGround[2].transform.position.z)
+        {
+            BackGround[1].transform.position = BackGround[3].transform.position;
+        }
     }
 }
